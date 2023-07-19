e0cd8c74f9852c99c9c1886da4c5df65a82f93e7
kulisak12 <kulisak12lp@gmail.com>
2023-07-19 09:10:33 +0200

    Create the minimal cover

--- src/batchup/backup.py
+++ src/batchup/backup.py
@@ -11,30 +11,78 @@ from batchup.patterns import matches_any
 logger: logging.Logger
 
 
-def list_unignored_files_in_tree(
+def cover_tree_without_ignored(
     path: str, ignored: Set[re.Pattern]
 ) -> Generator[str, None, None]:
-    """Generates paths to unignored files."""
+    """Generates a disjoin cover of the non-ignored subtree of path.
+
+    Creates the smallest possible cover:
+    lists the entire directory if nothing in subtree is ignored.
+    """
+    for entry in _cover_tree_without_ignored(path, ignored):
+        if entry is not None:
+            yield entry
+
+
+def _cover_tree_without_ignored(
+    path: str, ignored: Set[re.Pattern]
+) -> Generator[Optional[str], None, None]:
+    """Internal version of `cover_tree_without_ignored`.
+    Yields `None` for skipped entries.
+    """
     # allow patterns to filter dirs by a trailing slash
     path = end_dir_with_slash(path)
-
     if matches_any(path, ignored):
+        yield None
         logger.log(10, f"Ignored: {path}")
     elif os.path.islink(path):
+        yield None
         logger.log(20, f"Skipping symlink: {path}")
     elif os.path.isfile(path):
         yield path
     elif os.path.isdir(path):
-        for entry in os.listdir(path):
-            entry_path = os.path.join(path, entry)
-            yield from list_unignored_files_in_tree(entry_path, ignored)
+        yield from _cover_dir_tree_without_ignored(path, ignored)
     else:
         raise BatchupError(f"Can't process path: {path}")
 
 
-def list_outdated_files_in_tree(
-    source: str, target: str
-) -> Generator[str, None, None]:
+def _cover_dir_tree_without_ignored(
+    dir: str, ignored: Set[re.Pattern]
+) -> Generator[Optional[str], None, None]:
+    """Version of `cover_tree_without_ignored` for a directory."""
+    postponed: List[str] = []
+    has_skipped = False
+
+    # don't yield results immediately, save them to postponed
+    # if nothing in subtree is ignored, discard the results and return dir
+    # if something is ignored, flush the previous results
+    # (which should come first) and then only then continue traversing
+    for entry_result in _list_dir_covers(dir, ignored):
+        if entry_result is None:
+            yield None  # flush postponed from callers
+            if not has_skipped:
+                has_skipped = True
+                yield from postponed
+        elif has_skipped:
+            yield entry_result
+        else:
+            postponed.append(entry_result)
+
+    # can return the entire directory
+    if not has_skipped:
+        yield dir
+
+
+def _list_dir_covers(
+    dir: str, ignored: Set[re.Pattern]
+) -> Generator[Optional[str], None, None]:
+    """Generates a concatenation of covers of directory entries."""
+    for entry in os.listdir(dir):
+        entry_path = os.path.join(dir, entry)
+        yield from cover_tree_without_ignored(entry_path, ignored)
+
+
+def list_outdated_files(source: str, target: str) -> Generator[str, None, None]:
     """Generates paths to outdated files.
 
     Source must be a regular file or a directory.
@@ -48,7 +96,7 @@ def list_outdated_files_in_tree(
 
     # directory => recurse
     for entry in os.listdir(source):
-        yield from list_outdated_files_in_tree(
+        yield from list_outdated_files(
             os.path.join(source, entry),
             os.path.join(target, entry)
         )
