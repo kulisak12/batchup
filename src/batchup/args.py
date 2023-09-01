644a537a5bb0fb5976e44d72d552cb9e7d3152a5
kulisak12 <kulisak12lp@gmail.com>
2023-09-01 20:44:42 +0200

    Add rules file description

--- src/batchup/args.py
+++ src/batchup/args.py
@@ -18,6 +18,7 @@ class Namespace(argparse.Namespace):
 
 def parse_args() -> Namespace:
     parser = argparse.ArgumentParser()
+    parser.formatter_class = argparse.RawTextHelpFormatter
 
     parser.add_argument("-n", "--dry-run", action="store_true", help="Don't copy anything, just show what would be done.")
     parser.add_argument("-l", "--keep-symlinks", action="store_true", help="Keep symbolic links. The target filesystem must support them.")
@@ -27,6 +28,15 @@ def parse_args() -> Namespace:
 
     parser.add_argument("rules", help="Path to the rules file.")
     parser.add_argument("backup_dir", help="Path to the backup directory.")
+
+    parser.epilog = """
+The rules file contains several sections preceded by a header. Each section
+consists of glob patterns, one per line. The following headers are recognized:
+- [copy]: Files and directories that will be backed up.
+- [ignore]: Patterns that will not be backed up.
+- [zip]: Directories that will be backed up as zip files.
+- [exec]: Scripts that will be executed before the backup starts.
+"""
 
     args = Namespace()
     parser.parse_args(namespace=args)
