c0665e3581389f9149baea8b7a52ec8be34c42a5
kulisak12 <kulisak12lp@gmail.com>
2023-09-03 20:54:04 +0200

    Add derivation checks

--- src/batchup/target.py
+++ src/batchup/target.py
@@ -1,6 +1,14 @@
 import os
 from typing import Callable, Optional
 
+from batchup import BatchupError
+
+
+class DerivationError(BatchupError):
+    """Error raised during target derivation."""
+    pass
+
+
 TargetDerivation = Callable[[str], str]
 
 
@@ -19,9 +27,22 @@ def get_target_derivation(source_root: str, backup_dir: str) -> TargetDerivation
 
     The source tree starting at `source_root` is mounted at `backup_dir`.
     """
-    return lambda source: os.path.join(
-        backup_dir, os.path.relpath(source, source_root)
-    )
+    # prevent same-file errors and repeated backups
+    if _is_subdir(source_root, backup_dir):
+        raise BatchupError(
+            f"Root can't be a subdirectory of backup dir"
+        )
+
+    def derivation(source: str) -> str:
+        # source_root must be a prefix of source
+        if not _is_subdir(source, source_root):
+            raise DerivationError(
+                f"Source {source} is not in tree of {source_root}"
+            )
+        relpath = os.path.relpath(source, source_root)
+        return os.path.join(backup_dir, relpath)
+
+    return derivation
 
 
 def get_default_target_derivation(backup_dir: str) -> TargetDerivation:
@@ -31,3 +52,10 @@ def get_default_target_derivation(backup_dir: str) -> TargetDerivation:
     """
     root = os.path.abspath(os.sep)
     return get_target_derivation(root, backup_dir)
+
+
+def _is_subdir(path: str, parent: str) -> bool:
+    """Returns True if `path` is a subdirectory of `parent`."""
+    path = os.path.abspath(path)
+    parent = os.path.abspath(parent)
+    return path.startswith(parent)
